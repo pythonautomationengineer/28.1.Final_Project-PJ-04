ebb9412a1ccfca1f5ba2573d34ab6eb42886adb2
Tagir <qaautomationengineer@mail.ru>
2023-07-09 13:05:16 +0300

    Финальная версия conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
-from selenium import webdriver
 import pytest
-
+from selenium import webdriver
 
 @pytest.fixture
 def browser():
+    browser = webdriver.Chrome()
     browser = webdriver.Chrome()
     browser.maximize_window()
     yield browser
