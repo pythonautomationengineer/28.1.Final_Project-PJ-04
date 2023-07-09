2b30ec93969a8d967bc0ebd0a82c6be530751cfc
Tagir <qaautomationengineer@mail.ru>
2023-07-09 12:55:49 +0300

    Вторая глобавльная версия. Улучшены генеративные функции в классе FakePerson.py

--- conftest.py
+++ conftest.py
@@ -1,21 +1,10 @@
-import pytest
 from selenium import webdriver
-
-
-# @pytest.fixture
-# def browser():
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--headless')  # запуск браузера без GUI
-#
-#     browser = webdriver.Chrome(executable_path='chrome.exe', options=options)  # драйвер находится в Path на Windows
-#     browser.maximize_window()
-#     yield browser
-#     browser.quit()
+import pytest
 
 
 @pytest.fixture
 def browser():
-    browser = webdriver.Chrome(executable_path='chrome.exe')
+    browser = webdriver.Chrome('C:\\Users\\Tagir\\Desktop\\chromedriver\\chromedriver.exe')
     browser.maximize_window()
     yield browser
     browser.delete_all_cookies()
