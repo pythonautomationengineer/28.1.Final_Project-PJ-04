9d23265fba706e80461f3603ce78e08eece82df5
Tagir <qaautomationengineer@mail.ru>
2023-07-11 18:33:41 +0300

    Добавлен assert при наличии каптчи на сайте

--- tests/test_phone_tabs.py
+++ tests/test_phone_tabs.py
@@ -1,3 +1,4 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,6 +15,12 @@ def test_phone_tabs(browser):
     wait = WebDriverWait(browser, 7)
     email_button = wait.until(EC.visibility_of_element_located(Selectors.TAB_PHONE_BUTTON))
     email_button.click()
+
+    try:
+        captcha = browser.find_element(*Selectors.CAPTCHA_TEXT)
+        assert not captcha.is_displayed(), 'Каптча на сайте! Придется разок войти руками в ЛК, а после запустить тест'
+    except NoSuchElementException:
+        pass
 
     username_input = browser.find_element(*Selectors.USERNAME_INPUT)
     password_input = browser.find_element(*Selectors.PASSWORD_INPUT)
