f9dd43f67abb6c899ca2248dfa047a796452a5f1
Tagir <qaautomationengineer@mail.ru>
2023-07-11 19:00:01 +0300

    Отключены расширения, логи, по необходимости можно раскомментировать --headless

--- conftest.py
+++ conftest.py
@@ -1,11 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def browser():
-    browser = webdriver.Chrome()
+    # Создание объекта ChromeOptions
+    chrome_options = Options()
+
+    # Отключение логов
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-logging")
+
+    # Отключение расширений
+    chrome_options.add_argument("--disable-extensions")
+
+    # Включение хром-драйвера без графического интерфейса
+    # chrome_options.add_argument("--headless")
+
+    browser = webdriver.Chrome(chrome_options)
     browser.maximize_window()
+
     yield browser
     browser.delete_all_cookies()
     browser.quit()
