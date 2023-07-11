17c7b609e8929cc84806938eafa0f3b1944cf412
Tagir <qaautomationengineer@mail.ru>
2023-07-11 18:11:44 +0300

    Добавлен Assertion при наличии каптчи

--- tests/test_add_patronymic_name.py
+++ tests/test_add_patronymic_name.py
@@ -1,6 +1,7 @@
 from selenium.webdriver import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 
 from Classes.CSS_Selectors import Selectors
 from Classes.FakePerson import FakePerson
@@ -16,12 +17,19 @@ def test_login(browser):
     email_button = wait.until(EC.visibility_of_element_located(Selectors.TAB_EMAIL_BUTTON))
     email_button.click()
 
+    try:
+        captcha = browser.find_element(*Selectors.CAPTCHA_TEXT)
+        assert not captcha.is_displayed(), 'Каптча на сайте! Придется разок войти руками в ЛК, а после запустить тест'
+    except NoSuchElementException:
+        pass
+
     username_input = browser.find_element(*Selectors.USERNAME_INPUT)
     password_input = browser.find_element(*Selectors.PASSWORD_INPUT)
 
     username_input.send_keys(email_valid)
     password_input.send_keys(password)
 
+    # Кнопка "Войти"
     login_button = browser.find_element(*Selectors.LOGIN_BUTTON)
     login_button.click()
 
