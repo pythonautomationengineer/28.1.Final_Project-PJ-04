abaa02599eabe7a2ed77de31cc94342383fd32f1
Tagir <qaautomationengineer@mail.ru>
2023-07-11 18:19:22 +0300

    Добавлен Assertion при наличии каптчи

--- tests/test_change_of_first_and_last_name.py
+++ tests/test_change_of_first_and_last_name.py
@@ -1,3 +1,4 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,14 +17,20 @@ def test_login(browser):
     email_button = wait.until(EC.visibility_of_element_located(Selectors.TAB_EMAIL_BUTTON))
     email_button.click()
 
+    try:
+        captcha = browser.find_element(*Selectors.CAPTCHA_TEXT)
+        assert not captcha.is_displayed(), 'Каптча на сайте! Придется разок войти руками в ЛК, а после запустить тест'
+    except NoSuchElementException:
+        pass
+
     username_input = browser.find_element(*Selectors.USERNAME_INPUT)
     password_input = browser.find_element(*Selectors.PASSWORD_INPUT)
 
     username_input.send_keys(email_valid)
     password_input.send_keys(password)
 
+    # Кнопка "Войти"
     login_button = browser.find_element(*Selectors.LOGIN_BUTTON)
-
     login_button.click()
 
     # Ожидание кнопки изменения ФИО
