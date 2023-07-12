ad39cce067104f8bad98b2766e85212190f556e0
Tagir <qaautomationengineer@mail.ru>
2023-07-12 12:56:53 +0300

    Добавлен новый селектор "CREDENTIALS" в assert. Оптимизирован import

--- tests/test_phone_tabs.py
+++ tests/test_phone_tabs.py
@@ -1,10 +1,9 @@
 from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from settings import link, phone_valid, password
 from Classes.CSS_Selectors import Selectors
+from settings import link, phone_valid, password
 
 
 def test_phone_tabs(browser):
@@ -31,8 +30,7 @@ def test_phone_tabs(browser):
     login_button = browser.find_element(*Selectors.LOGIN_BUTTON)
     login_button.click()
 
-    assert browser.find_element(By.CSS_SELECTOR, '#app > main > div > div.home > div.base-card.home__info-card > '
-                                                 'h3:nth-child(2)').text == 'Учетные данные'
+    assert browser.find_element(*Selectors.CREDENTIALS).text == 'Учетные данные'
     print()
     print()
     print('Вход в кабинет выполнен, значит автоматическая смена таба произошла')
