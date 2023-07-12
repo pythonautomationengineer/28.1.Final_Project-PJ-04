8f73204765e1ac29b66c7587fb2f07dd0a1feb2e
Tagir <qaautomationengineer@mail.ru>
2023-07-12 14:32:14 +0300

    В класс добавлены 10 вариантов текста для ассертов в тестах

--- Classes/Data_for_Assert.py
+++ Classes/Data_for_Assert.py
@@ -1,2 +1,23 @@
 class DataForAssert:
+    """Текст для ассертов в текстах"""
+
     CAPTCHA_INFO = 'Каптча на сайте! Придется разок войти руками в ЛК, а после запустить тест'
+
+    TOAST_TEXT = 'Изменение ФИО'
+
+    ERROR_LOGIN_AND_PASSWORD_TEXT = 'Неверный логин или пароль'
+
+    BIG_LETTER_TEXT_IN_PASSWORD = 'Пароль должен содержать хотя бы одну заглавную букву'
+
+    ONLY_LATIN_LETTER_TEXT_IN_PASSWORD = 'Пароль должен содержать только латинские буквы'
+
+    PREVIOUSLY_USED_PASSWORD = 'Этот пароль уже использовался, укажите другой пароль'
+
+    VERY_SHORT_PASSWORD = 'Длина пароля должна быть не менее 8 символов'
+
+    CREDENTIALS = 'Учетные данные'
+
+    PASSWORD_ICON_ATTRIBUTE_1 = 'password'
+    PASSWORD_ICON_ATTRIBUTE_2 = 'text'
+
+    PASSWORDS_DO_NOT_MATCH = 'Пароли не совпадают'
