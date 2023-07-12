6e277383c0bd4edc587ca02cef52459aa53b9296
Tagir <qaautomationengineer@mail.ru>
2023-07-12 17:12:31 +0300

    Блок кода try except теперь импортируется в виде функции handle_captcha

--- tests/test_change_of_first_and_last_name.py
+++ tests/test_change_of_first_and_last_name.py
@@ -1,4 +1,3 @@
-from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from Classes.CSS_Selectors import Selectors
 from Classes.Data_for_Assert import DataForAssert
 from Classes.FakePerson import FakePerson
+from Classes.try_except_exception import handle_captcha
 from settings import link, email_valid, password
 
 
@@ -18,11 +18,8 @@ def test_login(browser):
     email_button = wait.until(EC.visibility_of_element_located(Selectors.TAB_EMAIL_BUTTON))
     email_button.click()
 
-    try:
-        captcha = browser.find_element(*Selectors.CAPTCHA_TEXT)
-        assert not captcha.is_displayed(), DataForAssert.CAPTCHA_INFO
-    except NoSuchElementException:
-        pass
+    # Если каптча присутствует на странице, то функция handle_captcha выдаст AssertionError, иначе выполнится без ошибок
+    handle_captcha(browser)
 
     username_input = browser.find_element(*Selectors.USERNAME_INPUT)
     password_input = browser.find_element(*Selectors.PASSWORD_INPUT)
