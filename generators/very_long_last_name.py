646fef238cec17a2f72bf332bf6a8cd7f3925f8f
Tagir <123125929+pythonautomationengineer@users.noreply.github.com>
2023-07-13 19:22:34 +0300

    Delete very_long_last_name.py

--- generators/very_long_last_name.py
+++
@@ -1,9 +0,0 @@
-def very_long_last_name_generation():
-    main_str = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
-    first_symbol = set(main_str).pop().upper()
-    for i in set(main_str):
-        first_symbol += i
-        if len(first_symbol) > 30:
-            break
-
-    return first_symbol
