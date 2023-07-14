9e132566bb45309ed5444a5bd5b0572aa182ce31
Tagir <qaautomationengineer@mail.ru>
2023-07-14 23:05:43 +0300

    В класс добавлена функция

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -4,10 +4,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from Сlasses.CSS_Selectors import Selectors
-from Сlasses.Data_for_Assert import DataForAssert
 from Сlasses.FakePerson import FakePerson
-from Сlasses.Characters_generator import CharactersGenerator
 from settings import link, unused_phone, password, email_valid
+from Сlasses.Data_for_Assert import DataForAssert
+from Сlasses.Characters_generator import CharactersGenerator
 
 
 # запустить все тесты в этом модуле
@@ -287,3 +287,53 @@ class TestCreateAccount:
         print(
             f"Текст '{text_information.text}' найден на странице в 2 местах после клика по кнопке "
             f"'Зарегистрироваться'. Значит валидация на заглавную букву пароля работает")
+
+    @staticmethod
+    @pytest.mark.negarive
+    def test_latin_password(browser):
+        """Появление сообщения в форме регистрации с текстом "Пароль должен содержать только латинские буквы" под полем
+        "Пароль" и "Подтверждение пароля при введении 9 символов кириллицы"""
+        browser.get(link)
+
+        # Явное ожидание ссылки с текстом "Зарегистрироваться" на главной странице
+        wait = WebDriverWait(browser, 7)
+        registration = wait.until(EC.visibility_of_element_located(Selectors.LINK_WITH_THE_TEXT_REGISTER))
+        registration.click()
+
+        # Явное ожидание кнопки с текстом "Зарегистрироваться" на странице регистрации
+        WebDriverWait(browser, 5)
+        browser.find_element(*Selectors.USER_CONCLUSION)
+
+        # Имя
+        browser.find_element(*Selectors.FIRST_NAME_INPUT).send_keys(FakePerson.generate_first_name_of_man(""))
+
+        # Фамилия
+        browser.find_element(*Selectors.LAST_NAME_INPUT).send_keys(FakePerson.generate_last_name_of_man(""))
+
+        # Телефон
+        browser.find_element(*Selectors.ADDRESS_INPUT).send_keys(unused_phone)
+
+        # Пароли
+        browser.find_element(*Selectors.REGISTRATION_PASSWORD).send_keys(
+            CharactersGenerator.not_latin_password_generator())
+        browser.find_element(*Selectors.REGISTRATION_PASSWORD_CONFIRM).send_keys(
+            CharactersGenerator.not_latin_password_generator())
+
+        browser.find_element(*Selectors.THE_REGISTER_BUTTON).click()
+
+        # Явное ожидание текста "Пароль должен содержать только латинские буквы" под полем 'Пароль'"
+        WebDriverWait(browser, 2)
+        text_information = browser.find_element(*Selectors.TEXT_REFERENCE_OF_PASSWORD)
+
+        # Явное ожидание текста "Пароль должен содержать только латинские буквы" под полем 'Подтверждение пароля'"
+        WebDriverWait(browser, 2)
+        text_information_2 = browser.find_element(*Selectors.TEXT_REFERENCE_OF_PASSWORD_CONFIRMATION)
+
+        assert text_information.text == DataForAssert.ONLY_LATIN_LETTER_TEXT_IN_PASSWORD \
+               and text_information_2.text == DataForAssert.ONLY_LATIN_LETTER_TEXT_IN_PASSWORD
+
+        print()
+        print()
+        print(
+            f"Текст '{text_information.text}' найден на странице в 2 местах после клика по кнопке "
+            f"'Зарегистрироваться'. Значит валидация на латинские символы пароля работает.")
