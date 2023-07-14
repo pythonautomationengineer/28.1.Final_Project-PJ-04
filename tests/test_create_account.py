3c706198c513415f2fe2d62bb98cc27234c960a3
Tagir <qaautomationengineer@mail.ru>
2023-07-14 22:51:00 +0300

    В класс добавлена функция

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from Сlasses.CSS_Selectors import Selectors
 from Сlasses.FakePerson import FakePerson
-from settings import link, email_valid, password
+from settings import link, password, unused_phone, email_valid
 
 
 # запустить все тесты в этом модуле
@@ -142,3 +142,45 @@ class TestCreateAccount:
         print()
         print(f"По кнопке 'Войти' в модальном окне под заголовком 'Учетная запись уже используется' можно вернуться на "
               f"главную страницу")
+
+    @staticmethod
+    @pytest.mark.positive
+    def test_valid_registration(browser):
+        """Отправка сайтом кода подтверждения регистрации при всех введенных валидных данных, которые ранее
+        не были использованы"""
+        browser.get(link)
+
+        # Явное ожидание ссылки с текстом "Зарегистрироваться" на главной странице
+        wait = WebDriverWait(browser, 5)
+        registration = wait.until(EC.visibility_of_element_located(Selectors.LINK_WITH_THE_TEXT_REGISTER))
+        registration.click()
+
+        # Явное ожидание кнопки с текстом "Зарегистрироваться" на странице регистрации
+        WebDriverWait(browser, 5)
+        browser.find_element(*Selectors.USER_CONCLUSION)
+
+        # Имя
+        browser.find_element(*Selectors.FIRST_NAME_INPUT).send_keys(FakePerson.generate_first_name_of_man(''))
+
+        # Фамилия
+        browser.find_element(*Selectors.LAST_NAME_INPUT).send_keys(FakePerson.generate_last_name_of_man(''))
+
+        # Номер
+        browser.find_element(*Selectors.ADDRESS_INPUT).send_keys(unused_phone)
+
+        browser.find_element(*Selectors.REGISTRATION_PASSWORD).send_keys(password)
+        browser.find_element(*Selectors.REGISTRATION_PASSWORD_CONFIRM).send_keys(password)
+
+        # Кнопка "Зарегистрироваться"
+        browser.find_element(*Selectors.THE_REGISTER_BUTTON).click()
+
+        # Явное ожидание текста о том, что код подтверждения телефона был отправлен по указанному номеру
+        WebDriverWait(browser, 5)
+        text_information = browser.find_element(*Selectors.CONFIRMATION_PHONE_TEXT)
+
+        assert text_information.is_displayed()
+
+        print()
+        print()
+        print(f"Текст '{text_information.text}' найден на странице, значит пользователю "
+              f"код подтверждения телефона был отправлен")
