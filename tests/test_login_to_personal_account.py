f02691b4323c3273e43aea8a6e2a5e71f277d5e7
Tagir <qaautomationengineer@mail.ru>
2023-07-14 22:32:02 +0300

    В класс добавлен новый метод

--- tests/test_login_to_personal_account.py
+++ tests/test_login_to_personal_account.py
@@ -172,3 +172,31 @@ class TestLogin:
         print()
         print(f'Вход в кабинет не выполнен, так как на странице появился текст ошибки "{error_message.text}". '
               f'Смена таба не произошла.')
+
+    @staticmethod
+    @pytest.mark.positive
+    def test_phone_tabs(browser):
+        """Смена таба выбора аутентификации при вводе телефона в табе 'Почта'"""
+        browser.get(link)
+
+        # Явное ожидание таба с текстом "Почта"
+        wait = WebDriverWait(browser, 10)
+        email_button = wait.until(EC.visibility_of_element_located(Selectors.TAB_PHONE_BUTTON))
+        email_button.click()
+
+        # Если каптча присутствует на странице, то функция handle_captcha выдаст AssertionError,
+        # иначе выполнится без ошибок
+        handle_captcha(browser)
+
+        # Телефон и пароль
+        browser.find_element(*Selectors.USERNAME_INPUT).send_keys(phone_valid)
+        browser.find_element(*Selectors.PASSWORD_INPUT).send_keys(password)
+
+        # Кнопка "Войти"
+        browser.find_element(*Selectors.LOGIN_BUTTON).click()
+
+        assert browser.find_element(*Selectors.CREDENTIALS).text == DataForAssert.CREDENTIALS
+        print()
+        print()
+        print(f'Вход в кабинет выполнен. Текст "{DataForAssert.CREDENTIALS}" найден на странице, значит автоматическая '
+              f'смена таба произошла.')
