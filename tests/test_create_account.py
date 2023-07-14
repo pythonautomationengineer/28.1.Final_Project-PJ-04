edfabba2d4253c2d4392c3abb645e7b92ff9c5aa
Tagir <qaautomationengineer@mail.ru>
2023-07-14 23:15:02 +0300

    В класс добавлена функция

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -1,13 +1,15 @@
 import pytest
 
+from Сlasses.Characters_generator import CharactersGenerator
+
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from Сlasses.CSS_Selectors import Selectors
-from Сlasses.FakePerson import FakePerson
-from settings import link, unused_phone, password, email_valid
 from Сlasses.Data_for_Assert import DataForAssert
-from Сlasses.Characters_generator import CharactersGenerator
+from Сlasses.FakePerson import FakePerson
+from Сlasses.Characters_generator import CharactersGenerator as Cg
+from settings import link, unused_phone, password, email_valid
 
 
 # запустить все тесты в этом модуле
@@ -337,3 +339,45 @@ class TestCreateAccount:
         print(
             f"Текст '{text_information.text}' найден на странице в 2 местах после клика по кнопке "
             f"'Зарегистрироваться'. Значит валидация на латинские символы пароля работает.")
+
+    @staticmethod
+    @pytest.mark.negarive
+    def test_pass_dont_match(browser):
+        """Выводится "Пароли не совпадают" под полем "Подтверждение пароля", если пользователь ввел разные пароли
+        при регистрации"""
+        browser.get(link)
+
+        # Явное ожидание ссылки с текстом "Зарегистрироваться" на главной странице
+        wait = WebDriverWait(browser, 7)
+        registration = wait.until(EC.visibility_of_element_located(Selectors.LINK_WITH_THE_TEXT_REGISTER))
+        registration.click()
+
+        # Явное ожидание кнопки с текстом "Зарегистрироваться" на странице регистрации
+        WebDriverWait(browser, 5)
+        browser.find_element(*Selectors.USER_CONCLUSION)
+
+        # Имя
+        browser.find_element(*Selectors.FIRST_NAME_INPUT).send_keys(FakePerson.generate_first_name_of_man(""))
+
+        # Фамилия
+        browser.find_element(*Selectors.LAST_NAME_INPUT).send_keys(FakePerson.generate_last_name_of_man(""))
+
+        browser.find_element(*Selectors.ADDRESS_INPUT).send_keys(unused_phone)
+
+        # Пароли
+        browser.find_element(*Selectors.REGISTRATION_PASSWORD).send_keys(Cg.p_password_generator())
+        browser.find_element(*Selectors.REGISTRATION_PASSWORD_CONFIRM).send_keys(Cg.p2_password_generator())
+
+        # Кнопка "Зарегистрироваться"
+        browser.find_element(*Selectors.THE_REGISTER_BUTTON).click()
+
+        # Явное ожидание текста "Пароли не совпадают" под полем 'Подтверждение пароля'"
+        WebDriverWait(browser, 2)
+        text_information_2 = browser.find_element(*Selectors.TEXT_REFERENCE_OF_PASSWORD_CONFIRMATION)
+
+        assert text_information_2.text == DataForAssert.PASSWORDS_DO_NOT_MATCH
+
+        print()
+        print()
+        print(f"Текст '{text_information_2.text}' найден на странице под полем 'Подтверждение пароля' после клика "
+              f"по кнопке 'Зарегистрироваться'. Значит валидация на подтверждение пароля работает")
