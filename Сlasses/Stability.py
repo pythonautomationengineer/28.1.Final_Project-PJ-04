46c346a9c15968d9a4a5fd28062d6b2f6aae640d
Tagir <qaautomationengineer@mail.ru>
2023-07-16 15:34:51 +0300

    Добавлены новые классы по определению OS, имени пользователя, горячим клавишам

--- Сlasses/Stability.py
+++ Сlasses/Stability.py
@@ -1,18 +1,20 @@
+import platform
+import getpass
 from selenium.common.exceptions import NoSuchElementException
 from Сlasses.CSS_Selectors import Selectors
 from Сlasses.Data_for_Assert import DataForAssert
-import platform
-import getpass
+from selenium.webdriver import Keys
 
 
 class StabilityTimes:
-    """Установка времени явного и неявного ожидания"""
+    """Установка времени явного и неявного ожидания для всех тестов"""
     explicit_wait = 10  # Явное ожидание
     implicit_wait = 3  # Неявное ожидание
 
 
 class Captcha:
     """Функции, связанные с появлением каптчи"""
+
     @staticmethod
     def handle_captcha(browser):
         """Вызвать AssertionError, если на сайте каптча мешает выполнению теста """
@@ -23,16 +25,34 @@ class Captcha:
             pass
 
 
-class OperationSystemAndUserName:
+class OSandUserName:
     """Определение ОС, на которой исполняется код и имени пользователя"""
+    all_support_os = ["Windows", 'macOS', 'Linux']
+    apple_os = 'macOS'
+    microsoft_os = 'Windows'
+    linux = 'Linux'
+
     @staticmethod
     def os():
-        """Операционная система"""
+        """Операционная система пользователя, запускающего тест"""
         operating_system = platform.system()
         return operating_system
 
     @staticmethod
-    def user():
-        """Имя пользователя"""
+    def user_login():
+        """Имя пользователя, запускающего тест"""
         username = getpass.getuser()
         return username
+
+    @staticmethod
+    def hotkeys(user_os):
+        """Задание горячих клавиш для очистки символов в инпутах на разных OS"""
+        win = Keys.CONTROL + "a", Keys.DELETE
+        apple = Keys.COMMAND + "a", Keys.DELETE
+        linux = Keys.CONTROL + "a", Keys.DELETE
+        if user_os == OSandUserName.microsoft_os:
+            return win
+        elif user_os == OSandUserName.apple_os:
+            return apple
+        elif user_os == OSandUserName.linux:
+            return linux
