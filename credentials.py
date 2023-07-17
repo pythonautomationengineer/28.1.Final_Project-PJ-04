da6e8f4b0972fd23ec6f7509f894dc5511b92bda
Tagir <qaautomationengineer@mail.ru>
2023-07-17 15:23:52 +0300

    Добавлены комментарии

--- credentials.py
+++ credentials.py
@@ -1,7 +1,12 @@
+# Адрес сайта
 link = "https://b2c.passport.rt.ru/"
+
+# Валидные данные для входа
 phone_valid = '79934150596'
-unused_phone = '79274292618'
 email_valid = 'testuser-sf@yandex.ru'
 login = 'rtkid_1687043939523'
-old_password = 'Qq23456789#QR'
 password = 'Qq23456789#QRC'
+
+# Невалидные данные для входа
+unused_phone = '79274292618'
+old_password = 'Qq23456789#QR'
