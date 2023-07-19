49d7ed89c7d4a6510ca16ea3611b3ee2df7164d9
Tagir <qaautomationengineer@mail.ru>
2023-07-19 22:48:46 +0300

    Незначительные изменения в аннотациях функций

--- Common_actions/login_to_personal_account_action_helpers.py
+++ Common_actions/login_to_personal_account_action_helpers.py
@@ -15,56 +15,56 @@ class LoginHelpers:
 
     @staticmethod
     def login_button(browser):
-        # Клик по кнопке "Войти"
+        """Клик по кнопке 'Войти'"""
         browser.find_element(*Selectors.LOGIN_BUTTON).click()
 
     @staticmethod
     def valid_phone_and_valid_password(browser):
-        # Ввод валидного телефона и валидного пароля
+        """Ввод валидного телефона и валидного пароля"""
         browser.find_element(*Selectors.USERNAME_INPUT).send_keys(phone_valid)
         browser.find_element(*Selectors.PASSWORD_INPUT).send_keys(password)
 
     @staticmethod
     def valid_email_and_valid_password(browser):
-        # Ввод валидного email и пароля
+        """Ввод валидного email и пароля"""
         browser.find_element(*Selectors.USERNAME_INPUT).send_keys(email_valid)
         browser.find_element(*Selectors.PASSWORD_INPUT).send_keys(password)
 
     @staticmethod
     def valid_email_and_not_valid_password(browser):
-        # Ввод валидного телефона и устаревшего пароля
+        """Ввод валидного телефона и устаревшего пароля"""
         browser.find_element(*Selectors.USERNAME_INPUT).send_keys(phone_valid)
         browser.find_element(*Selectors.PASSWORD_INPUT).send_keys(old_password)
 
     @staticmethod
     def valid_login_and_valid_password(browser):
-        # Ввод валидного логина и пароля
+        """Ввод валидного логина и валидного пароля"""
         browser.find_element(*Selectors.USERNAME_INPUT).send_keys(login)
         browser.find_element(*Selectors.PASSWORD_INPUT).send_keys(password)
 
     @staticmethod
     def wait_tab_phone_button_and_click(browser):
-        # Явное ожидание таба с текстом "Телефон" и клик по нему
+        """Явное ожидание таба с текстом "Телефон" и клик по нему"""
         wait = WebDriverWait(browser, StabilityTimes.explicit_wait)
         phone_button = wait.until(ec.visibility_of_element_located(Selectors.TAB_PHONE_BUTTON))
         phone_button.click()
 
     @staticmethod
     def wait_tab_email_button_and_click(browser):
-        # Явное ожидание таба с текстом "Почта" и клик по нему
+        """Явное ожидание таба с текстом "Почта" и клик по нему"""
         wait = WebDriverWait(browser, StabilityTimes.explicit_wait)
         email_button = wait.until(ec.visibility_of_element_located(Selectors.TAB_EMAIL_BUTTON))
         email_button.click()
 
     @staticmethod
     def wait_tab_login_button_and_click(browser):
-        # Явное ожидание таба с текстом "Логин" и клик по нему
+        """Явное ожидание таба с текстом "Логин" и клик по нему"""
         wait = WebDriverWait(browser, StabilityTimes.explicit_wait)
         login_button = wait.until(ec.visibility_of_element_located(Selectors.TAB_LOGIN_LOGIN))
         login_button.click()
 
     @staticmethod
     def the_text_of_the_login_error(browser):
-        # Ожидание текста ошибки входа в ЛК
+        """Ожидание текста ошибки входа в ЛК"""
         wait = WebDriverWait(browser, StabilityTimes.explicit_wait)
         wait.until(ec.visibility_of_element_located(Selectors.FORM_ERROR_MESSAGE))
